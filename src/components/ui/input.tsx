3c8dd7f50abb2b10a73ac8da59bcf16547424146
Zuzanna Gasior <zuzanna.gasior9@gmail.com>
2025-05-11 13:38:52 +0200

    fix: fix input lint errors

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
